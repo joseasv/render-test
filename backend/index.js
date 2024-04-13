dcda1bea8ac82afc2151b3c00ec3474c0de2f5ec
José Sánchez <joseasv@outlook.com>
2024-04-13 06:58:32 -0400

    Added the next callback to delete endpoint

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,7 @@ app.post("/api/notes", (request, response) => {
   });
 });
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
     .then((result) => {
       response.status(204).end();
